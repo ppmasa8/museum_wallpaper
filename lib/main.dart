d3b2573f9accacaf986045966cbce6ef41494cbe
ppmasa8 <kiyokiyopip56@gmail.com>
2022-02-05 02:13:50 +0900

    Convert from Matrial to GetMaterial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'view/screens/homeview.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
