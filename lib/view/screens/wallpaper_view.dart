38d78e423f1a6e244d060eee9f7395772c5be1fd
ppmasa8 <kiyokiyopip56@gmail.com>
2022-02-06 01:04:03 +0900

    Add get back

--- lib/view/screens/wallpaper_view.dart
+++ lib/view/screens/wallpaper_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:metropolitan_museum/models/metropolitanMuseum.dart';
+import 'package:metropolitan_museum/view/utils/helpers/color_helper.dart';
 
 class WallpaperView extends StatelessWidget {
   final MetropolitanMuseum wallpaper;
@@ -10,14 +12,27 @@ class WallpaperView extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Image.network(wallpaper.primaryImage.toString()),
-          Column(
-            children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.arrow_back_ios)
-              )
-            ],
+          Image.network(
+            wallpaper.primaryImage.toString(),
+            height: Get.height,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            child: Column(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: whitecolor,
+                    )
+                )
+              ],
+            ),
           )
         ],
       ),
