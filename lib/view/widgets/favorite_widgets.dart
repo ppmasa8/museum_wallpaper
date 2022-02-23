6a072fdbe4646ff4ea3e6deb2275cb1e5320c868
ppmasa8 <kiyokiyopip56@gmail.com>
2022-02-24 00:43:09 +0900

    Fix auto-format

--- lib/view/widgets/favorite_widgets.dart
+++ lib/view/widgets/favorite_widgets.dart
@@ -3,43 +3,44 @@ import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:metropolitan_museum/controllers/favorite_controller.dart';
 import 'package:metropolitan_museum/models/metropolitanMuseum.dart';
-import 'package:metropolitan_museum/view/utils/constants/const.dart';
 import 'package:metropolitan_museum/view/utils/helpers/color_helper.dart';
 
 class FavoriteWidgets extends StatelessWidget {
-  const FavoriteWidgets ({Key? key}) : super(key: key);
+  const FavoriteWidgets({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 30, right: 30, bottom: 0, left: 30),
-      child: GetBuilder<FavoriteController>(
-        init: FavoriteController(),
-        builder: (controller) => ValueListenableBuilder(
-            valueListenable: controller.favoriteBox!.listenable(),
-            builder: (context, Box<MetropolitanMuseum> box, child) {
-              final List<String> keys = box.keys.cast<String>().toList();
-              return keys.isEmpty
-                  ? const EmptyList()
-                  : GridView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 30,
-                      crossAxisSpacing: 30,
-                      childAspectRatio: 2 / 2.7,
-                    ),
-                    itemCount: keys.length,
-                    itemBuilder: (context, index) {
-                      final String key = keys[index];
-                      final MetropolitanMuseum? wallpaper = box.get(key);
-                      return Container(
-                        color: pinkcolor,
-                        child: Image.network(wallpaper!.primaryImage.toString()),
-                      );
-                    });
-        }),)
-    );
+        padding: const EdgeInsets.only(top: 30, right: 30, bottom: 0, left: 30),
+        child: GetBuilder<FavoriteController>(
+          init: FavoriteController(),
+          builder: (controller) => ValueListenableBuilder(
+              valueListenable: controller.favoriteBox!.listenable(),
+              builder: (context, Box<MetropolitanMuseum> box, child) {
+                final List<String> keys = box.keys.cast<String>().toList();
+                return keys.isEmpty
+                    ? const EmptyList()
+                    : GridView.builder(
+                        physics: const BouncingScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 30,
+                          crossAxisSpacing: 30,
+                          childAspectRatio: 2 / 2.7,
+                        ),
+                        itemCount: keys.length,
+                        itemBuilder: (context, index) {
+                          final String key = keys[index];
+                          final MetropolitanMuseum? wallpaper = box.get(key);
+                          return Container(
+                            color: pinkcolor,
+                            child: Image.network(
+                                wallpaper!.primaryImage.toString()),
+                          );
+                        });
+              }),
+        ));
   }
 }
 
@@ -53,4 +54,3 @@ class EmptyList extends StatelessWidget {
     );
   }
 }
-
