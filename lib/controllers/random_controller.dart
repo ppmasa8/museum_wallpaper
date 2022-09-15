6bcd3707d2d029fcc75d04964eadae7f9eb5393c
ppmasa8 <kiyokiyopip@gmail.com>
2022-09-16 02:26:58 +0900

    Added randomizer for randomlist

--- lib/controllers/random_controller.dart
+++ lib/controllers/random_controller.dart
@@ -1,33 +1,38 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:metropolitan_museum/controllers/base_controller.dart';
 import 'package:metropolitan_museum/models/metropolitanMuseum.dart';
 import 'package:metropolitan_museum/services/rest_api_service.dart';
 import 'package:metropolitan_museum/view/utils/constants/const.dart';
+import 'package:metropolitan_museum/view/utils/constants/objectIDs.dart';
 
 class RandomController extends BaseController {
   final RestApiService _restApiService = RestApiService();
   final ScrollController randomScrollController = ScrollController();
   List<MetropolitanMuseum> randomList = [];
+  var intArray = [];
 
   // These number is the result of a search for the word "old" on MetropolitanMuseumApi.
   Future<void> getListOfRandom() async {
     setState(true);
+    randomizer();
     randomList += await _restApiService
-        .convertJsonToObjectOnlyImgAndWikiURL(api + "${328905}");
+        .convertJsonToObjectOnlyImgAndWikiURL(api + "${intArray[0]}");
     randomList += await _restApiService
-        .convertJsonToObjectOnlyImgAndWikiURL(api + "${322890}");
+        .convertJsonToObjectOnlyImgAndWikiURL(api + "${intArray[1]}");
     randomList += await _restApiService
-        .convertJsonToObjectOnlyImgAndWikiURL(api + "${44292}");
+        .convertJsonToObjectOnlyImgAndWikiURL(api + "${intArray[2]}");
     randomList += await _restApiService
-        .convertJsonToObjectOnlyImgAndWikiURL(api + "${751543}");
+        .convertJsonToObjectOnlyImgAndWikiURL(api + "${intArray[3]}");
     randomList += await _restApiService
-        .convertJsonToObjectOnlyImgAndWikiURL(api + "${717577}");
+        .convertJsonToObjectOnlyImgAndWikiURL(api + "${intArray[4]}");
     randomList += await _restApiService
-        .convertJsonToObjectOnlyImgAndWikiURL(api + "${328902}");
+        .convertJsonToObjectOnlyImgAndWikiURL(api + "${intArray[5]}");
     randomList += await _restApiService
-        .convertJsonToObjectOnlyImgAndWikiURL(api + "${322272}");
+        .convertJsonToObjectOnlyImgAndWikiURL(api + "${intArray[6]}");
     randomList += await _restApiService
-        .convertJsonToObjectOnlyImgAndWikiURL(api + "${327457}");
+        .convertJsonToObjectOnlyImgAndWikiURL(api + "${intArray[7]}");
     setState(false);
   }
 
@@ -52,6 +57,14 @@ class RandomController extends BaseController {
     }
     randomList.addAll(wallpapers);
     setBottomState(false);
+  }
+
+  void randomizer() {
+    var n = objectIDs.length;
+    var rng = Random();
+    for (var i = 0; i < 8; i++) {
+      intArray.add(objectIDs[rng.nextInt(n) - 1]);
+    }
   }
 
   @override
