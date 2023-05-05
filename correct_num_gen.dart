ba0419e176f98f9201490ee6cc9ae3909aa4b855
ppmasa8 <kiyokiyopip56@gmail.com>
2023-05-06 03:51:29 +0900

    Small fix

--- correct_num_gen.dart
+++ correct_num_gen.dart
@@ -6,9 +6,12 @@ import './lib/view/utils/constants/const.dart';
 import 'lib/view/utils/constants/landscape_objectIDs.dart';
 
 void main() async {
+  // File name will be changed as needed.
+  var file = await File('./tmp/test1.txt').create();
+
   // Categories will be changed as needed.
   var size = landscapeObjectIDs.length;
-  var dataList = [];
+
   for (var i = 0; i < size; i++) {
     // Categories will be changed as needed.
     Uri uri = Uri.parse(api + "${landscapeObjectIDs[i]}");
@@ -19,17 +22,14 @@ void main() async {
       try {
         var imageResponse = await http.get(Uri.parse(imageData));
         if (imageResponse.statusCode == 200) {
-          dataList.add(landscapeObjectIDs[i]);
+          await file.writeAsString(landscapeObjectIDs[i].toString());
         }
-        print(dataList);
+        print(landscapeObjectIDs[i]);
       } catch (e) {
         print(e);
       }
     }
     sleep(const Duration(seconds: 2));
   }
-  // File name will be changed as needed.
-  var file = await File('./tmp/test1.txt').create();
-  await file.writeAsString(dataList.toString());
   print("done");
 }
